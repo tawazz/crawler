5a2270652633812a4b06e969810d1d74f8e7450c
tawazz <tawanda.nyakudjga@gmail.com>
2016-06-30 16:22:19 +0800

    Download files on windows

--- app/routes/home/home.php
+++ app/routes/home/home.php
@@ -30,18 +30,20 @@
     }
   })->name('content');
 
-  $app->post("/download",function() use($app){
-    $body = $app->request->getBody();
-    $body = json_decode($body);
-    $url = $body->url;
+  $app->get("/download/:id",function($id) use($app){
 
-    set_time_limit(0); // unlimited max execution time
-    $episode_url = "http://streaming.fshare.to/api_process.php?link=V25JMlFiNnBRZUlXOGxxTm1OVEtMb3BBTG82b0E1N0JkMzk1MVVzQ0htOG5Wa0k1N0phWW9aNzlEbnhlcDBOVEtBcUhQbWxRWXFGbm1pQTJqaGRyYngyQWJycllSVldoVzhaSnM0OERHbHNPQlM0amo2OFlURHBUZElTWW9ZR3Q2eEo0VXQ3TjJsOUJPTnNhMytaVGY0U3JiM04xbmgyeDVyTFpod0x3bnR1NXhNcksrWHJEcFErU25WdzhOQTJJQ21iSnpOQW1XSTA3SXJkZDRyTXNQMitmZm10T3VMYU9IcmV1bG5PeGpFcmZRa3d4QTN0Q2xkc3czSkNPWmlxS2pWdC82eVh5N01TbTZpTXp3ajA2c0QwdnBWaUVjRFR2cEU5RDRoai9MdlhEaGRxYTc4ZkhwalFRNWlSekhRa2Jrd3NTZWE5VGlvMnNTZUhrdGZUTUNiWHR2UXF5Y2ZUKzVlVHhyMGR0aGNKenp4ZWsyenR2MGpIaVZYUlhodnVxd2ZhNTNoakc2ZEJmbWZEY1ZKUk03S1BjK1kwM0Z3Si92ZjE0TjFHeFc1SFBCZ3g3Z2N6T09hVC93Tm1rTTNsOHdWMjA3WTRvV0QyVlU3NER3VkF2NmlHZHZXVVNJSmxrcnc2Nmd4cmVMZVllY3Z1S3Z4VFBHU25BK0ZoNlN3M1RWZllNdzdOM2M2ZlY2eWJOQnBJZ05NRSs1amJRSWwzQlRQeWIxQXJCUGxIR3RpcVJQdkFaaGtlbkNITCtONkJ0eHhnaEVRRnV1dlAvNkdGbUs2Zm5LQT09";
-    $client = $app->goute;
-    $myFile = fopen('Downloads/movie2.mp4',"wb") or die("Problems");
-    $response = $client->request('GET', $episode_url,['sink'=>$myFile,'stream' => true,'progress' => function ($dl_total_size, $dl_size_so_far, $ul_total_size, $ul_size_so_far) {
-         echo $dl_size_so_far / $dl_total_size  * 100;
-    }]);
-    fclose($myFile);
+    $link = $app->Link->find($id);
+    $movie = $app->Movie->find($link->movie_id);
+    $movieFolder = str_replace([':','?',"/",' '],"_",$movie->title);
+    $title = str_replace([':','?',"/",' '],"_",$link->title);
+    $folder = 'C:/wamp/www/crawler/Downloads/'.$movieFolder;
+    if(!file_exists($folder)){
+      mkdir($folder);
+    }
+    $cmd = 'wget -O '.$folder.'/'.$title.'.mp4 --no-cookies --header "Cookie:'. $movie->ads_hash."=". $movie->ads_token.'" --header "Cookie:__cfduid = d24101684cf98efe599dfcd70370065ec1467193730" --no-check-certificate '.$link->url .'2>&1';
+
+    $WshShell = new COM("WScript.Shell");
+    $oExec = $WshShell->Run($cmd, 0, false);
+    echo "downloading...";
   })->name("download");
  ?>
